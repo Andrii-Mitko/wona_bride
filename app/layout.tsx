86bfbdddb74009512a9377ee69b9f7920a2cdc7e
Andrii-Mitko <andreymit123@gmail.com>
2026-08-07 07:30:42 +0300

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -205,6 +205,21 @@ export default function RootLayout({
             }}
           />
 
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-KBZCSKVN4R"
+            strategy="afterInteractive"
+          />
+
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-KBZCSKVN4R');
+  `}
+          </Script>
+
           {children}
           <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID!} />
         </TanStackProvider>
