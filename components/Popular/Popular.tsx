b5ec022ee0fc992b9d2423a49c7f804d7289d2dd
Andrii-Mitko <andreymit123@gmail.com>
2026-08-17 21:49:16 +0300

    fix RevealOnScroll 2

--- components/Popular/Popular.tsx
+++ components/Popular/Popular.tsx
@@ -1,7 +1,6 @@
 import DressGrid from "@/components/DressGrid/DressGrid";
 import { getDresses } from "@/lib/api/dresses";
 import css from "./Popular.module.css";
-import RevealOnScroll from "@/components/RevealOnScroll/RevealOnScroll";
 
 export default async function Popular() {
   const { dresses: popularDresses } = await getDresses({
@@ -12,21 +11,19 @@ export default async function Popular() {
   return (
     <section className={css.section} id="popular">
       <div className={css.container}>
-        <RevealOnScroll>
-          <div className={css.header}>
-            <hr className="section-divider" />
-            <p className={css.label}>Обрані моделі</p>
+        <div className={css.header}>
+          <hr className="section-divider" />
+          <p className={css.label}>Обрані моделі</p>
 
-            <h2 className={css.title}>Популярні сукні</h2>
+          <h2 className={css.title}>Популярні сукні</h2>
 
-            <p className={css.subtitle}>
-              Найулюбленіші моделі наших клієнток. Витончені фасони для
-              особливих моментів.
-            </p>
-          </div>
+          <p className={css.subtitle}>
+            Найулюбленіші моделі наших клієнток. Витончені фасони для особливих
+            моментів.
+          </p>
+        </div>
 
-          <DressGrid dresses={popularDresses} />
-        </RevealOnScroll>
+        <DressGrid dresses={popularDresses} />
       </div>
     </section>
   );
